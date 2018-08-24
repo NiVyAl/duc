e72e8ad665bf840569365b8cf3e72b84a0c29d55
NiVyAl <nikitavisoc@gmail.com>
2018-08-24 23:26:45 +0300

    1.3 delete text button, search tip

---
+++ ducScript.js
@@ -0,0 +1,6 @@
+var deleteSearchBtn = document.getElementById('deleteSearch');
+var searchText = document.getElementById('searchText');
+
+var deleteSearchText = function() {
+    searchText.value = "";
+}
