3f4fdeaa6de2b0186456e5419d80497f2b710909
NiVyAl <nikitavisoc@gmail.com>
2018-08-25 22:17:16 +0300

    1.35 tips, features

--- ducScript.js
+++ ducScript.js
@@ -4,3 +4,9 @@ var searchText = document.getElementById('searchText');
 var deleteSearchText = function() {
     searchText.value = "";
 }
+
+
+var tips = document.querySelectorAll('.tip a');
+console.log(tips);
+
+var featuresBtn = document.querySelector('[for="features"]');
